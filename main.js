aa0366d071e27d9b6b0e2e6daccf66bfdda302c2
Deonte McCoy <deontemccoy19@gmail.com>
2022-09-13 09:28:23 -0500

    Create modules for gameBoard and display, and create factory function for players

---
+++ main.js
@@ -0,0 +1,16 @@
+// Stores the gameboard into an object
+const gameBoard = (() => {
+    const gameObj = {
+        boardArray: []
+    };
+})();
+
+// Stores player info into an object
+const player = (name, marker) => {
+    return {name, marker};
+};
+
+// Controls what is displayed on the gameboard
+const displayController = (() => {
+
+})();
