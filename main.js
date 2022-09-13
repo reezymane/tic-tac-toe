294d4b5c8d89429cb0c5b6c5ed535ed74a1f9b4c
Deonte McCoy <deontemccoy19@gmail.com>
2022-09-13 11:16:40 -0500

    Create even listener for start button to request and store player info

--- main.js
+++ main.js
@@ -12,6 +12,26 @@ const player = (name, marker) => {
 
 // Controls what is displayed on the gameboard
 const displayController = (() => {
+    // Requests and stores info for Players 1 & 2
+    let player1;
+    let player2;
+    const start = document.querySelector('.start');
+    start.addEventListener('click', () => {
+        const name = prompt('Player 1, enter your name!')
+        const marker = prompt('Would you like to be X or O?').toUpperCase();
+       
+        player1 = player(name, marker);
+        
+        const name2 = prompt('Player 2, enter your name!')
+        if (marker === 'X') {
+            alert(name2 + '\'s marker is O!');
+            player2 = player(name2, 'O');
+        } else {
+            alert(name2 + '\'s marker is X!');
+            player2 = player(name2, 'X');
+        };
+    });
+    
     gameBoard.boardArray.forEach((div) => {
         div.addEventListener('click', () => {
             // Renders players gameboard placement
