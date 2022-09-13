36801a2e0735df1425845a423c4f73d91759ae80
Deonte McCoy <deontemccoy19@gmail.com>
2022-09-13 10:19:49 -0500

    Create function to render player's gamerboard placement

--- main.js
+++ main.js
@@ -12,5 +12,13 @@ const player = (name, marker) => {
 
 // Controls what is displayed on the gameboard
 const displayController = (() => {
+    gameBoard.boardArray.forEach((div) => {
+        div.addEventListener('click', () => {
+            // Renders players gameboard placement
+            const playerMove = document.createElement('p');
+            playerMove.textContent = 'x';
 
+            div.appendChild(playerMove);
+        });
+    })
 })();
